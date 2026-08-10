5363d57d227438d05b1c0ddc6a19589dbf600a32
dufei <dufei@tap4fun.com>
2026-08-11 00:44:12 +0800

    test(exchange): consume restore plan public API

--- packages/exchange/type-tests/public-api.ts
+++ packages/exchange/type-tests/public-api.ts
@@ -1,11 +1,14 @@
 import {
+  buildLedgerRestorePlan,
   readPackage,
   runZipRuntimeProbe,
   ZIP_ENTRY_OPTIONS,
   ZIP_PROBE_FIXTURES,
   ZIP_READER_OPTIONS,
   ZIP_WORKER_OPTIONS,
+  type LedgerRestorePlan,
   type ReadLedgerPackage,
+  type Sha256Hex,
   type ZipRuntimeProbeResult,
 } from "@tenjin/exchange";
 
@@ -16,6 +19,11 @@ const checksSignature: true = ZIP_ENTRY_OPTIONS.checkSignature;
 
 const resultPromise: Promise<ZipRuntimeProbeResult> = runZipRuntimeProbe();
 const packagePromise: Promise<ReadLedgerPackage> = readPackage(fixtureBytes);
+const deterministicDigest: Sha256Hex = async () => "00".repeat(32);
+const restorePlanPromise: Promise<LedgerRestorePlan> = packagePromise.then(
+  (ledgerPackage) =>
+    buildLedgerRestorePlan(ledgerPackage, deterministicDigest),
+);
 
 void fixtureBytes;
 void workerEnabled;
@@ -23,3 +31,4 @@ void strictReader;
 void checksSignature;
 void resultPromise;
 void packagePromise;
+void restorePlanPromise;
